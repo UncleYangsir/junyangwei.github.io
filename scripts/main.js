d856155ab7fb81ad55a8bb0e6c929169ceb9d28e
UncleYangsir <30687851+UncleYangsir@users.noreply.github.com>
2020-08-19 12:53:06 +0800

    feat: add my personal web
    将我的个人网站添加到GitHub仓库

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// 修改大标题
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Jun Yang. Wei';
+
+// 点击事件
+let myHTML = document.querySelector('html');
+myHTML.onclick = function() {
+  alert('Hello.');
+};
